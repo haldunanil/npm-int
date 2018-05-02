d29ef61a5926ae2a0246f97a9746e086f35633ba
Haldun Anil <haldunanil@gmail.com>
2018-05-01 21:09:27 -0400

    Make alerts better

--- src/components/AlertSection/AlertSection.js
+++ src/components/AlertSection/AlertSection.js
@@ -3,29 +3,47 @@ import "./AlertSection.css";
 import { Alert, Container } from "reactstrap";
 
 class AlertSection extends Component {
-  render() {
-    return (
-      <div>
-        {this.props.data.sellingRsus.invalid ||
-        this.props.data.taxRate.invalid ? (
-          <Container style={{ paddingTop: 25 }}>
-            {this.props.data.sellingRsus.invalid ? (
-              <Alert color="danger">
-                You can sell between 0-{this.props.calculators.makeNumberMoreReadable(
-                  this.props.data.header.numRsusAvail
-                )}{" "}
-                RSUs.
-              </Alert>
-            ) : null}
-            {this.props.data.taxRate.invalid ? (
-              <Alert color="danger">
-                Your tax rate must be between 0-100%.
-              </Alert>
-            ) : null}
-          </Container>
-        ) : null}{" "}
-      </div>
+  makeRSUSaleAlert() {
+    const maxRsus = this.props.calculators.makeNumberMoreReadable(
+      this.props.data.header.numRsusAvail
     );
+
+    switch (maxRsus) {
+      case "0":
+        return <Alert color="danger">You don't have any RSUs to sell.</Alert>;
+      default:
+        return (
+          <Alert color="danger">You can sell between 0-{maxRsus} RSUs.</Alert>
+        );
+    }
+  }
+
+  makeTaxRateAlert() {
+    return <Alert color="danger">Your tax rate must be between 0-100%.</Alert>;
+  }
+
+  makeProcessingAlert() {
+    return <Alert color="info">Your check is being processed.</Alert>;
+  }
+
+  render() {
+    const displayAlerts =
+      this.props.data.sellingRsus.invalid ||
+      this.props.data.taxRate.invalid ||
+      this.props.data.header.orderStatus;
+
+    if (displayAlerts) {
+      return (
+        <Container style={{ paddingTop: 25 }}>
+          {this.props.data.sellingRsus.invalid ? this.makeRSUSaleAlert() : null}
+          {this.props.data.taxRate.invalid ? this.makeTaxRateAlert() : null}
+          {this.props.data.header.orderStatus === "processing"
+            ? this.makeProcessingAlert()
+            : null}
+        </Container>
+      );
+    }
+    return null;
   }
 }
 
